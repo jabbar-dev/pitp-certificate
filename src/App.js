05f0800cc47e39f357264eda25b664a280900ddd
Abdul Jabbar Shah <rashdijabbar@gmail.com>
2024-12-06 21:13:26 +0500

    changes a text

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
           type="text"
           value={certificateNumber}
           onChange={handleChange}
-          placeholder="Enter Your Student / Certificate ID"
+          placeholder="Enter Your Student / Certificate ID Jabbar"
           className="form-control me-2"
         />
         
